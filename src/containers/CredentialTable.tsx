d5a5922bf091b1c9b0a26421e3600625b1dce3e4
shubham.Affinidi <shubham.b@affinidi.com>
2021-08-05 12:41:45 +0530

    GDE-857 done

--- src/containers/CredentialTable.tsx
+++ src/containers/CredentialTable.tsx
@@ -4,72 +4,77 @@ import { useAuthentication } from "./Authentication";
 import { useGlobalTokenValue } from "./MessageListener";
 import CheckCircle from '../assets/images/icons/check_green_circle.svg'
 import CrossCircle from '../assets/images/icons/cross_red_circle.svg'
+import { verifyVC } from './../services/apiService';
 
 const CredentialTable = () => {
-    const { sdk } = useAuthentication();
-    const [
-      credentialShareResponseToken,
-      setCredentialShareResponseToken,
-    ] = useState<string[]>([]);
-    const [globalToken] = useGlobalTokenValue();
-    const [vcData, setVCData] = useState<any[]>([]);
-    useEffect(() => {
-      if (globalToken) {
-        setCredentialShareResponseToken(prevState => [...prevState, globalToken]);
-      }
-    }, [globalToken]);
+  const { sdk } = useAuthentication();
+  const [
+    credentialShareResponseToken,
+    setCredentialShareResponseToken,
+  ] = useState<string[]>([]);
+  const [globalToken] = useGlobalTokenValue();
+  const [vcData, setVCData] = useState<any[]>([]);
+  useEffect(() => {
+    if (globalToken) {
+      setCredentialShareResponseToken(prevState => [...prevState, globalToken]);
+    }
+  }, [globalToken]);
 
-    useEffect(() => {
-      const onValidate = async (token: string) => {
-        const result = await sdk!.verifyCredentialShareResponseToken(token);
-        const credentialType = result.suppliedCredentials[0].type[(result.suppliedCredentials[0].type.length)-1]
-        let drivingClass: string | undefined = undefined
-        if (credentialType === 'IDDocumentCredentialPersonV1') {
-          drivingClass = JSON.parse(result.suppliedCredentials[0].credentialSubject.data.hasIDDocument?.hasIDDocument.idClass).drivingClass;
+  useEffect(() => {
+    const onValidate = async (token: string) => {
+      let result = await sdk!.verifyCredentialShareResponseToken(token);
+      const verify_vc = await verifyVC({ verifiableCredentials: [result.suppliedCredentials[0]] });
+      if (verify_vc) {
+        result.isValid = verify_vc.isValid
+      }
+      const credentialType = result.suppliedCredentials[0].type[(result.suppliedCredentials[0].type.length) - 1]
+      let drivingClass: string | undefined = undefined
+      if (credentialType === 'IDDocumentCredentialPersonV1') {
+        drivingClass = JSON.parse(result.suppliedCredentials[0].credentialSubject.data.hasIDDocument?.hasIDDocument.idClass).drivingClass;
+      }
+
+      setVCData(prevState => {
+        return [...prevState, { token, validatedResult: result, drivingClass }]
+      })
+    }
+    if (credentialShareResponseToken) {
+      credentialShareResponseToken.map((token: string) => {
+        // Check if the vcData already has the token = means it was validated before
+        const existingData = vcData.filter(data => data.token == token)
+        if (existingData.length == 0) {
+          return onValidate(token);
         }
-        
-        setVCData(prevState => {
-          return [...prevState, {token, validatedResult: result, drivingClass}]
-        })
-      }
-      if (credentialShareResponseToken) {
-        credentialShareResponseToken.map((token: string) => {
-          // Check if the vcData already has the token = means it was validated before
-          const existingData = vcData.filter(data => data.token == token)
-          if (existingData.length == 0){
-            return onValidate(token);
-          }
-        })
-      }
-    }, [credentialShareResponseToken, sdk])
+      })
+    }
+  }, [credentialShareResponseToken, sdk])
 
-    return <div>
-        <Table bordered>
-          <thead className="thead-light">
-            <tr>
-              <th>Index</th>
-              <th>Name</th>
-              <th>Driving Class</th>
-              <th>Validated</th>
-              {/* <th>Action</th> */}
+  return <div>
+    <Table bordered>
+      <thead className="thead-light">
+        <tr>
+          <th>Index</th>
+          <th>Name</th>
+          <th>Driving Class</th>
+          <th>Validated</th>
+          {/* <th>Action</th> */}
+        </tr>
+      </thead>
+      <tbody>
+        {vcData.map((data, index) => {
+          return (
+            <tr key={index}>
+              <th scope="row">{index + 1}</th>
+              <td>{data.validatedResult.suppliedCredentials[0].credentialSubject.data.givenName}</td>
+              <td>{data.drivingClass ? <p>{data.drivingClass}</p> : <p> No Driving Class </p>}</td>
+              <td>{data.validatedResult.isValid ? <img src={CheckCircle} alt='check' style={{ height: '28px' }} /> :
+                <img src={CrossCircle} alt='cross' style={{ height: '28px' }} />
+              }
+              </td>
             </tr>
-          </thead>
-          <tbody>
-            {vcData.map((data, index) => {
-              return (
-                <tr>
-                <th scope="row">{index+1}</th>
-                <td>{data.validatedResult.suppliedCredentials[0].credentialSubject.data.givenName}</td>
-                <td>{data.drivingClass ? <p>{data.drivingClass}</p>:<p> No Driving Class </p> }</td>
-                <td>{data.validatedResult.isValid ? <img src={CheckCircle} alt='check' style={{height: '28px'}} /> : 
-                    <img src={CrossCircle} alt='cross' style={{height: '28px'}} />
-                }
-                </td>
-              </tr>
-              )
-            })}
-          </tbody>
-        </Table>
-    </div>
+          )
+        })}
+      </tbody>
+    </Table>
+  </div>
 }
 export default CredentialTable;
--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -3,23 +3,44 @@ import config from "../config";
 
 const { apiKeyHash, env } = config
 const baseURL = `https://cloud-wallet-api.${env}.affinity-project.org/api/v1`
+const verifierBaseUrl = `https://affinity-verifier.${env}.affinity-project.org/api/v1`
 
 const cloudWalletApi = axios.create({
-	baseURL,
-	headers: {
-		'Api-Key': apiKeyHash,
-		'Content-Type': 'application/json',
-	},
+  baseURL,
+  headers: {
+    'Api-Key': apiKeyHash,
+    'Content-Type': 'application/json',
+  },
+});
+
+export const verifierApi = axios.create({
+  baseURL: verifierBaseUrl,
+  headers: {
+    'Api-Key': apiKeyHash,
+    'Content-Type': 'application/json',
+  },
 });
 
 export const signup = async (
-    username: string,
-    password: string
-  ) => {
-    const signUpParams = { username, password }
-    const { data: token } =  await cloudWalletApi.post('/users/signup', signUpParams)
+  username: string,
+  password: string
+) => {
+  const signUpParams = {
+    username, password, options: {
+      didMethod: "elem",
+      keyTypes: [
+        "rsa", 'bbs'
+      ]
+    }
+  }
+  const { data: token } = await cloudWalletApi.post('/users/signup', signUpParams)
 
-    return token
+  return token
+}
+
+export const verifyVC = async (input: any) => {
+  const { data } = await verifierApi.post('/verifier/verify-vcs', input)
+  return data;
 }
 
 export default cloudWalletApi
